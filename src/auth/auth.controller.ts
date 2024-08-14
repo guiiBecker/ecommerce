92221999017354766f742a4dc75091382ece689a
guiiBecker <guilhermegbecker@gmail.com>
2024-08-14 14:12:40 -0300

    feat(auth): login backend finished

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,27 +4,26 @@ import {
     HttpCode,
     HttpStatus,
     Post,
-    Req,
     Request,
     UseGuards,
   } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guards';
 import { AuthRequest } from './models/AuthRequest';
+import { IsPublic } from './decorators/is-public.decorator';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService){
-        @HttpCode(HttpStatus.OK)
-        @Post('login')
-        @UseGuards(LocalAuthGuard)
-        login (@Request() req: AuthController){
-            return this.authService.login(req.user)
-        }
+    constructor(private readonly authService: AuthService){}
 
-     }
+    @IsPublic()
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    async login (@Request() req: AuthRequest){
+        return this.authService.login(req.user);
     }
-
+    }
 
 
 
---
+++ src/auth/errors/unauthorized.error.ts
@@ -0,0 +1,1 @@
+export interface UnauthorizedError extends Error {}
