cf3c57342e6ff51df99e4baa6a8b4fab0a119e65
guiiBecker <guilhermegbecker@gmail.com>
2024-08-13 21:09:45 -0300

    feat(user): create implement

---
+++ src/user/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 2)
+    }
+    const CreatedUser = await this.prisma.user.create({ data:user })
+
+    return {
+      ...CreatedUser,
+      id: undefined,
+      password: undefined,
+    };
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
