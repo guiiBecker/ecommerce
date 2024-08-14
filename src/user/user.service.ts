eb23d61500b027dc7452221a5e102d7cb50b4c12
guiiBecker <guilhermegbecker@gmail.com>
2024-08-13 21:14:40 -0300

    feat(user): implement `findByEmail`

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -25,8 +25,10 @@ export class UserService {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findByEmail(email:string) {
+    return this.prisma.user.findUnique({
+      where: {email},
+    })
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
